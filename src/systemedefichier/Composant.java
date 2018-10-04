40cb95b182e8ff74b6f0f2b6547770f624c94f11
Utilisateur <user@debian.arcahe.ovh>
2018-10-04 11:06:44 +0200

    Ajout classe Main

--- src/systemedefichier/Composant.java
+++ src/systemedefichier/Composant.java
@@ -1,16 +1,13 @@
 package systemedefichier;
 
 public abstract class Composant {
-	 protected String nom;
+	 protected String _nom;
 	 
 	 protected Composant(String nom){
-		 this.nom=nom;
+		 this._nom=nom;
 	 }
 	 
 	 abstract protected  int getTaille();
 	 abstract protected String tostring();
-	 
-	 
-
 }
 
---
+++ src/systemedefichier/Main.java
@@ -0,0 +1,15 @@
+package systemedefichier;
+
+public class Main {
+	Repertoire r1 = new Repertoire("/");
+	Repertoire r2= new Repertoire("user");
+	Repertoire r3= new Repertoire("andy");
+	Repertoire r4= new Repertoire("ibrahim");
+	Repertoire r5= new Repertoire("aziz");
+	Repertoire r6= new Repertoire("system_fichier_ibra");
+	Repertoire r7= new Repertoire("system_fichier_andy");
+	Repertoire r8= new Repertoire("system_fichier_aziz");
+	Repertoire f1= new Fichier("");
+	Repertoire f2= new Fichier("user");
+	Repertoire f3= new Fichier("user");
+}
