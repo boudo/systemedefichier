38d193107eedbe675d8f9bf2597b36d5aa671cb1
Aziz EL ASSRI <aziz.el-assri@ens.uvsq.fr>
2018-10-12 12:55:54 +0200

    Ajout des commentaires JavaDoc

--- src/systemedefichier/Fichier.java
+++ src/systemedefichier/Fichier.java
@@ -2,16 +2,21 @@ package systemedefichier;
 
 public class Fichier extends Composant {
 	
-	
+	/**Taille du fichier*/
 	private int _taille;
 	
-	
+	/**
+	 * Constructeur de fichier
+	 * @param nom du fichier
+	 * @param taille du fichier
+	 */
 	public Fichier(String nom, int taille)
 	{
 		_nom = nom;
 		_taille = taille;
 	}
 	
+	@Override
 	public int getTaille()
 	{
 		return _taille;
