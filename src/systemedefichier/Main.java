382fbea903b287f83889a018389cf069d9388a4f
uvsq21603700 <ibrahim.boudo@ens.uvsq.fr>
2018-10-13 20:35:01 +0200

    modif du main

--- src/systemedefichier/Main.java
+++ src/systemedefichier/Main.java
@@ -1,38 +1,58 @@
 package systemedefichier;
 
 public class Main {
-	Repertoire r1 = new Repertoire("/");
-	Repertoire r2= new Repertoire("user");
-	Repertoire r3= new Repertoire("andy");
-	Repertoire r4= new Repertoire("ibrahim");
-	Repertoire r5= new Repertoire("aziz");
-	Repertoire r6= new Repertoire("system_fichier_ibra");
-	Repertoire r7= new Repertoire("system_fichier_andy");
-	Repertoire r8= new Repertoire("system_fichier_aziz");
-	Fichier f1= new Fichier("f1",10);
-	Fichier f2= new Fichier("f2",20);
-	Fichier f3= new Fichier("f3",30);
-	Fichier f4= new Fichier("f4",40);
-	Fichier f5= new Fichier("f5",50);
-	Fichier f6= new Fichier("f6",60);
-	Fichier f7= new Fichier("f7",70);
-	Fichier f8= new Fichier("f8",80);
-	Fichier f9= new Fichier("f9",90);
 	
-	r1.ajouter(r2);
-	r2.ajouter(r3);
-	r2.ajouter(r4);
-	r2.ajouter(r5);
-	r3.ajouter(r6);
-	r4.ajouter(r7);
-	r5.ajouter(r8);
+	public static void main(String[] args) {
+		
 	
-	System.out.println();
-	
-	
-	
-	
-	
+		Repertoire r1 = new Repertoire("/");
+		Repertoire r2= new Repertoire("user");
+		Repertoire r3= new Repertoire("andy");
+		Repertoire r4= new Repertoire("ibrahim");
+		Repertoire r5= new Repertoire("aziz");
+		Repertoire r6= new Repertoire("system_fichier_ibra");
+		Repertoire r7= new Repertoire("system_fichier_andy");
+		Repertoire r8= new Repertoire("system_fichier_aziz");
+		
+		Fichier f1= new Fichier("f1",10);
+		Fichier f2= new Fichier("f2",20);
+		Fichier f3= new Fichier("f3",30); // 100 dans andy
+		Fichier f4= new Fichier("f4",40);
+		
+		Fichier f5= new Fichier("f5",50);
+		Fichier f6= new Fichier("f6",60);
+		Fichier f7= new Fichier("f7",70); // 350 dans ibrahim
+		Fichier f8= new Fichier("f8",80);
+		Fichier f9= new Fichier("f9",90);
+		
+		r1.ajouterComposant(r2);
+		
+		r2.ajouterComposant(r3);
+		r2.ajouterComposant(r4);
+		r2.ajouterComposant(r5);
+		
+		r3.ajouterComposant(r7);
+		r3.ajouterComposant(f1);
+		r3.ajouterComposant(f2);
+		r7.ajouterComposant(f3);
+		r7.ajouterComposant(f4);
+		
+		r4.ajouterComposant(f5);
+		r4.ajouterComposant(r6);
+		r6.ajouterComposant(f6);
+		r6.ajouterComposant(f7);
+		r6.ajouterComposant(f8);
+		r6.ajouterComposant(f9);
+		System.out.println(r6.ajouterComposant(r6)); /////////////////////////// test
+		
+		r5.ajouterComposant(r8);
+		System.out.println(r8.ajouterComposant(r1)); /////////////////////////// test
+		System.out.println(r8.ajouterComposant(r3)); /////////////////////////// test
+		
+		
+		System.out.println(r1.getTaille());
+		
+	}
 	
 	
 }
